d251b9d0b6c4a93b380efb2a113879cff672f7ca
JHC21 <comma1010k@gmail.com>
2020-05-27 16:19:40 +0900

    create

--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -4,6 +4,11 @@ import kr.ac.konkuk.ccslab.cm.event.CMEvent;
 import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
 
 public class ServerHandler implements CMAppEventHandler {
+	private Server server;
+	
+	public ServerHandler(){
+		
+	}
 
 	@Override
 	public void processEvent(CMEvent arg0) {
