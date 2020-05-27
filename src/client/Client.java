107c09339380bd926f2f743b5af69ef64594c8a7
minjyo <minjyo8823@gmail.com>
2020-05-27 17:33:21 +0900

    clientController - Client - ClientHandler frame

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,12 +1,32 @@
 package client;
 
+import kr.ac.konkuk.ccslab.cm.event.CMUserEvent;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 
 public class Client {
 	private CMClientStub clientStub;
 	private ClientHandler clientHandler;
+	String session, group;
 
-	Client(){
-		
+	Client(String session, String group){
+		clientStub = new CMClientStub();
+		clientHandler = new ClientHandler(clientStub);
+		this.session = session;
+		this.group = group;
+	}
+	
+	//return "client application service" - interaction with CM
+	public CMClientStub getClientStub() {
+		return clientStub;
+	}		
+	
+	public ClientHandler getClientEventHandler() {
+		return clientHandler;
+	}
+	
+	public void multicast() {
+		CMUserEvent cme = new CMUserEvent();
+		//cme.setEventField(nDataType, strFieldName, strFieldValue);
+		clientStub.multicast(cme, session, group);
 	}
 }
--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -1,13 +1,34 @@
 package client;
 
 import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 
 public class ClientHandler implements CMAppEventHandler {
 
+	private CMClientStub clientStub;
+	
+	public ClientHandler(CMClientStub stub)
+	{
+		clientStub = stub;
+	}
+	
 	@Override
-	public void processEvent(CMEvent arg0) {
+	public void processEvent(CMEvent cme) {
 		// TODO Auto-generated method stub
+		switch(cme.getType())
+		{
+		case CMInfo.CM_USER_EVENT:
+			processUserEvent(cme);
+			break;
+		default:
+			return;
+		}
+		
+	}
+	
+	private void processUserEvent(CMEvent cme) {
 		
 	}
 
