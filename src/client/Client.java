45f81d408ccabf30a6c799452595ef8437d43180
JHC21 <comma1010k@gmail.com>
2020-06-03 19:38:35 +0900

    add Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,18 +1,30 @@
 package client;
 
+import kr.ac.konkuk.ccslab.cm.entity.CMGroup;
+import kr.ac.konkuk.ccslab.cm.entity.CMSession;
+import kr.ac.konkuk.ccslab.cm.entity.CMUser;
+import kr.ac.konkuk.ccslab.cm.event.CMDummyEvent;
 import kr.ac.konkuk.ccslab.cm.event.CMUserEvent;
+import kr.ac.konkuk.ccslab.cm.info.CMInteractionInfo;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class Client {
 	private CMClientStub clientStub;
 	private ClientHandler clientHandler;
+	int state, cmd;
+	
 	String session, group;
+	ArrayList<String> roomList;
 
 	Client(String session, String group){
 		clientStub = new CMClientStub();
 		clientHandler = new ClientHandler(clientStub);
 		this.session = session;
 		this.group = group;
+		
+		roomList = new ArrayList<String>();
 	}
 	
 	//return "client application service" - interaction with CM
@@ -26,7 +38,83 @@ public class Client {
 	
 	public void multicast() {
 		CMUserEvent cme = new CMUserEvent();
-		//cme.setEventField(nDataType, strFieldName, strFieldValue);
 		clientStub.multicast(cme, session, group);
 	}
+	
+	public void start() {
+		// TODO Auto-generated method stub		
+		boolean ret;
+		
+		clientStub.startCM();
+		
+		//get client name!
+		
+		ret = clientStub.joinSession("Session 1"); // Enter Lobby Session
+		if(ret)
+			System.out.println("successfully sent the session-join request.");
+		else
+			System.err.println("failed the session-join request!");
+		
+	}
+
+	public void setRoomList(String[] rooms) {
+		roomList.clear();
+		for(int i=0; i<rooms.length; i++) {
+			roomList.add(rooms[i]);
+		}
+	}
+	
+	public ArrayList<String> getRoomList() {
+		CMInteractionInfo interInfo = clientStub.getCMInfo().getInteractionInfo();
+		CMUser myself = interInfo.getMyself();
+		CMSession session = interInfo.findSession(myself.getCurrentSession());
+		Vector<CMGroup> roomGroup = session.getGroupList();
+		ArrayList<String> roomList = new ArrayList<String>();
+		
+		for(int i=0; i<roomGroup.size(); i++) {
+			roomList.add(roomGroup.elementAt(i).getGroupName());
+		}
+		
+		return roomList;
+	}
+	
+	public void createRoom(String roomName) {
+		
+		clientStub.leaveSession();
+		
+		clientStub.joinSession("Session2");
+		
+		
+		CMInteractionInfo interInfo = clientStub.getCMInfo().getInteractionInfo();
+		CMUser myself = interInfo.getMyself();
+		CMSession session = interInfo.findSession(myself.getCurrentSession());
+		
+		session.createGroup(roomName, myself.getHost(), myself.getUDPPort()); 
+		clientStub.changeGroup(roomName);
+	}
+	
+	public void enterRoom(String roomName) {
+		CMDummyEvent due = new CMDummyEvent();
+		due.setDummyInfo("Enter@#$" + roomName);
+		//
+		due.setSender("");
+		
+		clientStub.send(due, "SERVER");
+	}
+	
+	public void exitRoom() {
+		//session change
+		clientStub.leaveSession();
+		
+		clientStub.joinSession("Session1");
+	}
+
+	public void updateRoomList() {
+		// TODO Auto-generated method stub
+		state = 1; // lobby panel
+		cmd = 1; // update String types of room name arraylist
+	}
+	
+	
+	
 }
--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -1,12 +1,15 @@
 package client;
 
+import kr.ac.konkuk.ccslab.cm.event.CMDataEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMDummyEvent;
 import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 
 public class ClientHandler implements CMAppEventHandler {
-
+	private Client client;
 	private CMClientStub clientStub;
 	
 	public ClientHandler(CMClientStub stub)
@@ -14,14 +17,26 @@ public class ClientHandler implements CMAppEventHandler {
 		clientStub = stub;
 	}
 	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
 	@Override
 	public void processEvent(CMEvent cme) {
 		// TODO Auto-generated method stub
 		switch(cme.getType())
 		{
+		case CMInfo.CM_SESSION_EVENT:
+			processSessionEvent(cme);
+			break;
 		case CMInfo.CM_USER_EVENT:
 			processUserEvent(cme);
 			break;
+		case CMInfo.CM_DUMMY_EVENT:
+			processDummyEvent(cme);
+			break;
+		case CMInfo.CM_DATA_EVENT:
+			processDataEvent(cme);
 		default:
 			return;
 		}
@@ -31,5 +46,79 @@ public class ClientHandler implements CMAppEventHandler {
 	private void processUserEvent(CMEvent cme) {
 		
 	}
+	
+	private void processSessionEvent(CMEvent cme)
+	{
+		CMSessionEvent se = (CMSessionEvent)cme;
+		switch(se.getID())
+		{
+		case CMSessionEvent.REGISTER_USER_ACK:
+			if( se.getReturnCode() == 1 )
+			{
+				// user registration succeeded
+				System.out.println("User["+se.getUserName()+"] successfully registered at time["
+							+se.getCreationTime()+"].");
+			}
+			else
+			{
+				// user registration failed
+				System.out.println("User["+se.getUserName()+"] failed to register!");
+			}
+			break;
+		case CMSessionEvent.UNEXPECTED_SERVER_DISCONNECTION:
+			System.err.println("Unexpected disconnection from ["+se.getChannelName()
+					+"] with key["+se.getChannelNum()+"]!");
+			break;
+		case CMSessionEvent.INTENTIONALLY_DISCONNECT:
+			System.err.println("Intentionally disconnected all channels from ["
+					+se.getChannelName()+"]!");
+			break;
+		default:
+			return;
+		}
+	}
+	
+	private void processDummyEvent(CMEvent cme) {
+		CMDummyEvent due = (CMDummyEvent) cme;
+		
+		// Parse Client-Server Request
+		String inputStr[] = due.getDummyInfo().split("@#$");
+		
+		switch (inputStr[0]) {
+		case "okay":
+			clientStub.leaveSession();
+			clientStub.joinSession("Session2");
+			
+			clientStub.changeGroup(inputStr[1]);
+			break;
+		case "deny":
+			//Room Access Deny
+			client.updateRoomList();
+			break;
+		default:	
+			return;
+		}
+
+		return;
+	}
+	
+	private void processDataEvent(CMEvent cme)
+	{
+		CMDataEvent de = (CMDataEvent) cme;
+		switch(de.getID())
+		{
+		case CMDataEvent.NEW_USER:
+			System.out.println("["+de.getUserName()+"] enters group("+de.getHandlerGroup()+") in session("
+					+de.getHandlerSession()+").");
+			break;
+		case CMDataEvent.REMOVE_USER:
+			System.out.println("["+de.getUserName()+"] leaves group("+de.getHandlerGroup()+") in session("
+					+de.getHandlerSession()+").");
+			break;
+		default:
+			return;
+		}
+	}
+	
 
 }
